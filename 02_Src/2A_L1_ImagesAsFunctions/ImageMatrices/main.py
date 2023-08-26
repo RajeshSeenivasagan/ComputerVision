ee49ca00fdb0380ad514b4b6fa6094cfbbaca400
RajeshKumar <rajesh-kumar.seenivasagan@siemens.com>
2023-08-26 23:26:18 +0530

    Getting image intensity, datatype etc.,

--- 02_Src/2A_L1_ImagesAsFunctions/ImageMatrices/main.py
+++ 02_Src/2A_L1_ImagesAsFunctions/ImageMatrices/main.py
@@ -2,9 +2,19 @@ import cv2
 
 img = cv2.imread('D:/OnlineCourses/ComputerVision_Udacity/02_Repo/ComputerVision/01_Inputs/img_1.jpg')
 
-
-img[:,150] = (0, 0, 255)
 cv2.imshow('Display', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+image_dimensions = img.shape
+image_class = img.dtype
+#first dimension represents height, second dimension represents width, third dimension represents number of channels
+print(image_dimensions)
+#represents the data type of the pixel element
+print(image_class)
+
+
+modified_img = cv2.rectangle(img, (50, 200), (50, 200), (255, 0, 0), 1)
+cv2.imshow('Modified Display',modified_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
