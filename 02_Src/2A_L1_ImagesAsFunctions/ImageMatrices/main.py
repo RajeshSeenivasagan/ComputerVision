5280035a782439bd9ab36fbfee143ebbcf799f4d
RajeshKumar <rajesh-kumar.seenivasagan@siemens.com>
2023-08-29 17:13:01 +0530

    code to extract separate colour channels

--- 02_Src/2A_L1_ImagesAsFunctions/ImageMatrices/main.py
+++ 02_Src/2A_L1_ImagesAsFunctions/ImageMatrices/main.py
@@ -2,9 +2,6 @@ import cv2
 
 img = cv2.imread('D:/OnlineCourses/ComputerVision_Udacity/02_Repo/ComputerVision/01_Inputs/img_1.jpg')
 
-cv2.imshow('Display', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 image_dimensions = img.shape
 image_class = img.dtype
@@ -13,8 +10,17 @@ print(image_dimensions)
 #represents the data type of the pixel element
 print(image_class)
 
+img_red = img[:,:,0]
+img_green = img[:,:,1]
+img_blue = img[:,:,2]
 
-modified_img = cv2.rectangle(img, (50, 200), (50, 200), (255, 0, 0), 1)
-cv2.imshow('Modified Display',modified_img)
+image_red_dimensions = img_red.shape
+print(image_red_dimensions)
+
+cv2.imshow('original', img)
+cv2.imshow('Red_window', img_red)
+cv2.imshow('Green_window', img_green)
+cv2.imshow('Blue_window', img_blue)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
